baefcb6c6e307bbd23cfd0e699b14fc50e398bf3
chourabiaziz <chourabiaziz007@gmail.com>
2024-04-10 14:41:19 +0100

    Update page Index Contrat

---
+++ src/main/java/tn/esprit/controllers/ContratIndex.java
@@ -0,0 +1,58 @@
+package tn.esprit.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import tn.esprit.models.Contrat;
+import tn.esprit.services.ContratService;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ContratIndex  implements Initializable {
+
+    @FXML
+    private Label client;
+
+    @FXML
+    private Label couverture;
+
+    @FXML
+    private Label engagement;
+
+    @FXML
+    private Label prix;
+
+    private ContratService contratService;
+
+    private void afficherContrats() {
+        List<Contrat> listContrat = contratService.getAll();
+
+        // Clear existing labels
+        client.setText("");
+        couverture.setText("");
+        engagement.setText("");
+        prix.setText("");
+        if (!listContrat.isEmpty()) {
+            for (Contrat contrat : listContrat) {
+                // client.setText(client.getText() + contrat.getClient() + "\n");
+                couverture.setText(couverture.getText() + contrat.getCouverture() );
+                engagement.setText(engagement.getText() + contrat.getEngagement() );
+                prix.setText(prix.getText() + contrat.getPrix() );// Assuming there's a method getPrix() in Contrat class
+        }}
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        contratService = new ContratService();
+        afficherContrats();
+    }
+
+
+}
+
