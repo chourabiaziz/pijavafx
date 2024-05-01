afb90c388bceac86fc37b506da9dfb5508c78fd7
amal-selmi <amalselmi094@gmail.com>
2024-05-01 13:51:08 +0100

    intégrer api chatgpt

--- src/main/java/tn/esprit/controllers/AjouterPanne.java
+++ src/main/java/tn/esprit/controllers/AjouterPanne.java
@@ -6,13 +6,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import okhttp3.*;
 import tn.esprit.models.Panne;
 import tn.esprit.services.ServicePanne;
 
-import java.io.IOException;
-import java.util.Date;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class AjouterPanne implements Initializable {
@@ -29,7 +27,7 @@ public class AjouterPanne implements Initializable {
     @FXML
     private TextArea descriptionTextArea;
 
-    private static final String API_URL = "https://api.openai.com/v1/engines/davinci-codex/completions";
+//    private static final String API_URL = "https://api.openai.com/v1/engines/davinci-codex/completions";
 
 
     @Override
@@ -89,26 +87,26 @@ public class AjouterPanne implements Initializable {
         alert.showAndWait();
     }
 
-    @FXML
-    private void promptGpt() throws IOException {
-        OkHttpClient client = new OkHttpClient();
-
-        String prompt = "My name is";
-        String apiKey = "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";
-
-        MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\"prompt\": \"" + prompt + "\"}");
-
-        Request request = new Request.Builder()
-                .url(API_URL)
-                .post(body)
-                .addHeader("Authorization", "Bearer " + apiKey)
-                .addHeader("Content-Type", "application/json")
-                .build();
-
-        Response response = client.newCall(request).execute();
-        String responseBody = response.body().string();
-
-        System.out.println(responseBody);
-    }
+//    @FXML
+//    private void promptGpt() throws IOException {
+//        OkHttpClient client = new OkHttpClient();
+//
+//        String prompt = "My name is";
+//        String apiKey = "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";
+//
+//        MediaType mediaType = MediaType.parse("application/json");
+//        RequestBody body = RequestBody.create(mediaType, "{\"prompt\": \"" + prompt + "\"}");
+//
+//        Request request = new Request.Builder()
+//                .url(API_URL)
+//                .post(body)
+//                .addHeader("Authorization", "Bearer " + apiKey)
+//                .addHeader("Content-Type", "application/json")
+//                .build();
+//
+//        Response response = client.newCall(request).execute();
+//        String responseBody = response.body().string();
+//
+//        System.out.println(responseBody);
+//    }
 }
