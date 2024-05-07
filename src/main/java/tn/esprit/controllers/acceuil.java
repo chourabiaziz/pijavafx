9b032b8f165b0b29fc35504666d8aa257bb636fa
chourabiaziz <chourabiaziz007@gmail.com>
2024-05-07 11:04:41 +0100

    up

--- src/main/java/tn/esprit/controllers/acceuil.java
+++ src/main/java/tn/esprit/controllers/acceuil.java
@@ -1,7 +1,6 @@
 package tn.esprit.controllers;
 
-<<<<<<< HEAD
-import javafx.event.ActionEvent;
+ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
